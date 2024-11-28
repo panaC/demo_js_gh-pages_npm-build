1df701db8e92f7530a28e6bc2769f4e1a7c71ab0
Pierre Leroux <p.leroux@gmx.com>
2024-11-28 16:17:11 +0100

    rm src/main.js error build

--- src/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.echo = void 0;
-const echo = () => {
-    console.log("Hello World!");
-};
-exports.echo = echo;
